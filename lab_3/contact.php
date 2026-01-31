90fee2fa1819e63059edc1654d9dc117ef444436
Shy-Anna Lee <shyjah89@gmail.com>
2026-01-30 22:06:37 -0500

    Templates down

---
+++ lab_3/contact.php
@@ -0,0 +1,23 @@
+<?php require "includes/header.php";?>
+
+<main>
+    <h2>Contact Us</h2>
+    
+    <form action="process.php" method="post">
+        <label for="first_name">First Name</label>
+        <input type="text" id= "first_name" name="first_name" required>
+
+        <label for="last_name">Last Name</label>
+        <input type="text" id= "last_name" name="last_name" required>
+
+        <label for="first_name">First Name</label>
+        <input type="text" id= "first_name" name="first_name" required>
+
+        <label for="first_name">First Name</label>
+        <input type="text" id= "first_name" name="first_name" required>
+
+        <button type="submit">Submit</button>
+
+    </form>
+
+</main>
