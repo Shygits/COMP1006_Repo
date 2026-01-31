3a6ccdaa2ae959037710d18f84150e67eda5a0f8
Shy-Anna Lee <shyjah89@gmail.com>
2026-01-30 22:12:13 -0500

    contact.php done

--- lab_3/contact.php
+++ lab_3/contact.php
@@ -10,14 +10,16 @@
         <label for="last_name">Last Name</label>
         <input type="text" id= "last_name" name="last_name" required>
 
-        <label for="first_name">First Name</label>
-        <input type="text" id= "first_name" name="first_name" required>
+        <label for="email">Email</label>
+        <input type="email" id= "email" name="email" required>
 
-        <label for="first_name">First Name</label>
-        <input type="text" id= "first_name" name="first_name" required>
+        <label for="Message">Send Us Your Message</label>
+        <input type="text" id= "message" name="message" required>
 
         <button type="submit">Submit</button>
 
     </form>
 
 </main>
+
+<?php require "includes/footer.php";?>
