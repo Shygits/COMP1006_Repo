d97702276b547b0921a6556a4a98d7da867fc102
Shy-Anna Lee <shyjah89@gmail.com>
2026-02-12 10:06:58 -0500

    step 6

--- lab_4/subscribers.php
+++ lab_4/subscribers.php
@@ -37,7 +37,14 @@ $subscribers = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <tbody>
         <!-- 6.TODO: Loop through $subscribers and output each row -->
 <?php foreach ($subscribers as $subs): ?>
-
+  <tr>
+    <td><?=  htmlspecialchars($subs['id']) ?></td> ?>
+        <td><?=  htmlspecialchars($subs['first_name']) ?></td> ?>
+        <td><?=  htmlspecialchars($subs['last_name']) ?></td> ?>
+        <td><?=  htmlspecialchars($subs['email']) ?></td> ?>
+        <td><?=  htmlspecialchars($subs['subscribed_at']) ?></td> ?>
+  </tr>
+<?php endforeach; ?>
       </tbody>
     </table>
   <?php endif; ?>
